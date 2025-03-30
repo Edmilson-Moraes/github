154b3764c2d42e469dde14feafed4357b58e6d8a
Edmilson-Moraes <desouza.fn@gmail.com>
2025-03-30 14:03:42 -0300

    atualização

--- Curso Python/exercicios/ex_enunciados_2.py
+++ Curso Python/exercicios/ex_enunciados_2.py
@@ -3,6 +3,11 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex:
 Bom dia 0-11, boa tarde 12:17 e Boa noite 18:23
 """
-import datetime as dt
-hora = dt.datetime.now().hour
-print(f"São {hora} horas.", "bom dia" if hora < 12 else "boa tarde" if hora < 18 else "boa noite")
+
+hora = (input("Digite a hora em números inteiros: "))
+
+try:
+    hora = int(hora)
+    print(f"São {hora} horas.", "bom dia" if hora < 12 else "boa tarde" if hora < 18 else "boa noite")
+except:
+    print("Digite um número válido")
